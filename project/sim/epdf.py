1d3bf5632623fc42a0d7cac11aecbc01497b174b
Yuri Iozzelli <y.iozzelli@gmail.com>
2015-01-24 19:17:58 +0100

    nicer print

--- project/sim/epdf.py
+++ project/sim/epdf.py
@@ -24,6 +24,7 @@ for l,x in zip(labels,vecs):
     print("sample variance: "+str(numpy.var(x)))
     print("sample coefficient of variation: "+str(numpy.sqrt(numpy.var(x))/numpy.mean(x)))
     print("sample percentiles [25%,50%,75%,95%,99%]: "+str(numpy.percentile(x, [25,50,75,95,99])))
+    print()
 
     b = int(numpy.max(x)*10/bins)
     plt.hist(x,bins=b,normed=True,histtype='step',label=l)
